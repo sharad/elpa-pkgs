e9a94ff9c766922dbbed123a5367828979311b16
Sharad Pratap <110339+sharad@users.noreply.github.com>
2021-09-01 18:46:54 +0530

    in stone

--- lotus/occ/occ-select.el
+++ lotus/occ/occ-select.el
@@ -116,9 +116,6 @@ ACTION "
         (timeout   (or timeout occ-idle-timeout)))
     (helm-timed timeout (occ-helm-select-buffer)
       (occ-debug :debug "running occ-list-select")
-
-      ;; (error "Change code here for occ-return-tranform and occ-return-tranform and apply everywhere occ-build-helm-ap-transf")
-
       (let ((ap-normal (if return-transform (occ-return-tranform ap-normal) ap-normal)) ;as return value is going to be used.
             (ap-transf (if return-transform (occ-return-tranform ap-transf) ap-transf)))
         (occ-message "occ-list-select: ap-normal: %s" ap-normal)
