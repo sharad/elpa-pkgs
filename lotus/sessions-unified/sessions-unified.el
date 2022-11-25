d9e2c773d0d2ba5ead913501c590d0440215be05
Sharad Pratap <110339+sharad@users.noreply.github.com>
2022-11-25 22:52:00 +0530

    corretion

--- lotus/sessions-unified/sessions-unified.el
+++ lotus/sessions-unified/sessions-unified.el
@@ -190,13 +190,13 @@
 (defun sessions-unified-desktop-enable-restore-interrupting-feature-run-info ()
   (interactive)
   (if *sessions-unified-desktop-enable-restore-interrupting-feature-run-timer*
-      (let ((type (timer--idle-delay *sessions-unified-desktop-enable-restore-interrupting-feature-run-timer*))
-            (timesec (if *sessions-unified-desktop-enable-restore-interrupting-feature-run-timer*
-                         (if type
-                             (- (float-time (current-time))
-                                (float-time (timer--time *sessions-unified-desktop-enable-restore-interrupting-feature-run-timer*)))
-                           (float-time (timer--time *sessions-unified-desktop-enable-restore-interrupting-feature-run-timer*)))
-                       0)))
+      (let* ((type (timer--idle-delay *sessions-unified-desktop-enable-restore-interrupting-feature-run-timer*))
+             (timesec (if *sessions-unified-desktop-enable-restore-interrupting-feature-run-timer*
+                          (if type
+                              (- (float-time (current-time))
+                                 (float-time (timer--time *sessions-unified-desktop-enable-restore-interrupting-feature-run-timer*)))
+                            (float-time (timer--time *sessions-unified-desktop-enable-restore-interrupting-feature-run-timer*)))
+                        0)))
         (session-unfiy-notify "hooks will run %sly after %d" (or type "definite") timesec))
     (session-unfiy-notify "No timer present")))
 
