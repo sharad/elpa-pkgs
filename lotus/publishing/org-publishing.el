f4d614155c5d9e4dc38794f6e2b1e63d65c0bde2
Sharad Pratap <110339+sharad@users.noreply.github.com>
2023-03-07 20:45:12 +0530

    correction

--- lotus/publishing/org-publishing.el
+++ lotus/publishing/org-publishing.el
@@ -25,6 +25,7 @@
 ;;; Code:
 
 (provide 'org-publishing)
+
 
 (require 'cl)
 (require 'publishing)
@@ -32,6 +33,7 @@
 (when (featurep 'org-compact)
  (require 'org-compact))
 (require 'ox-publish)
+
 
 ;;;###autoload
 (defun org-publishing-created-contents-path (&optional path)
@@ -46,43 +48,38 @@
 ;;;###autoload
 (defun org-publishing-website-address (&optional localpath)
   (publishing-website-address 'org localpath))
+
 
 ;;;###autoload
 (defun* add-org-project (&rest project-spec)
   "Add org project."
-  (interactive
-   (let ((project-spec
-           (read-org-project-spec)))))
-
-  (when (and
-         (member (cl-first project-spec)
-                 (mapcar 'car org-publish-project-alist))
-         (or (not (called-interactively-p 'interactive))
-             (y-or-n-p (format "project %s already present, do you want to overwrite it?: " (cl-first project-spec))))
-         (remove-org-project project-spec)))
+  (interactive (let ((project-spec
+                      (read-org-project-spec)))))
+  (when (and (member (cl-first project-spec)
+                     (mapcar #'car org-publish-project-alist))
+             (or (not (called-interactively-p 'interactive))
+                 (y-or-n-p (format "project %s already present, do you want to overwrite it?: " (cl-first project-spec))))
+             (remove-org-project project-spec)))
   (add-to-list 'org-publish-project-alist project-spec t))
 
 ;;;###autoload
 (defun remove-org-project (&rest project-spec)
-  (interactive
-   (let ((project (ido-completing-read "Project: "
-                                       (mapcar 'car org-publish-project-alist))))
-     (if project
-         (list project)
-         (error "No project %s present" project))))
-  (let ((project
-         (cond
-           ((and (consp project-spec)
-                 (stringp (cl-first project-spec)))
-            (cl-first project-spec))
-           ((stringp project-spec)
-            project-spec)
-           (t nil))))
+  (interactive (let ((project (ido-completing-read "Project: "
+                                                   (mapcar 'car org-publish-project-alist))))
+                 (if project
+                     (list project)
+                   (error "No project %s present" project))))
+  (let ((project (cond ((and (consp project-spec)
+                             (stringp (cl-first project-spec)))
+                        (cl-first project-spec))
+                       ((stringp project-spec) project-spec)
+                       (t nil))))
     (if project
         (setq org-publish-project-alist
               (delete* project org-publish-project-alist
                        :key 'car
                        :test 'string-equal)))))
+
 
 ;; (defun* make-org-style-spec (org-dir publishing-path publishing-style publishing-url publishing-options)
 ;;   (interactive
@@ -108,26 +105,22 @@
 ;;     publishing-options)))
 
 ;;;###autoload
-(defun* make-org-project-spec (name &rest publishing-options)
-  (interactive
-   (let* ((name (read-from-minibuffer "Org Project Name: "))
-          (org-dir
-           (read-directory-name "Org Project Directory: " (org-publishing-created-contents-path name)))
-          (publishing-path
-           (read-directory-name
-            "Org Project Publishing Directory: "
-            (org-publishing-generated-contents-path
-                    (replace-regexp-in-string (org-publishing-created-contents-path ) ""
-                                              (if (consp org-dir) (cl-first org-dir) org-dir)))))
-          (publishing-options nil))
-     (list
-      name
-      :base-directory org-dir
-      :publishing-directory publishing-path
-      publishing-options)))
-  `(
-    ,name
-    ,@publishing-options))
+(defun* make-org-project-spec (name
+                               &rest
+                               publishing-options)
+  (interactive (let* ((name    (read-from-minibuffer "Org Project Name: "))
+                      (org-dir (read-directory-name "Org Project Directory: "
+                                                    (org-publishing-created-contents-path name)))
+                      (publishing-path (read-directory-name "Org Project Publishing Directory: "
+                                                            (org-publishing-generated-contents-path
+                                                             (replace-regexp-in-string (org-publishing-created-contents-path ) ""
+                                                                                       (if (consp org-dir) (cl-first org-dir) org-dir)))))
+                      (publishing-options nil))
+                 (list name
+                       :base-directory org-dir
+                       :publishing-directory publishing-path
+                       publishing-options)))
+  (cons name publishing-options))
 
 ;; (defun* read-org-style-spec ()
 ;;   (let* ((org-dir (read-directory-name "Org Project Directory: " (org-publishing-created-contents-path )))
@@ -143,18 +136,15 @@
 
 ;;;###autoload
 (defun* read-org-project-spec ()
-  (let* ((name (read-from-minibuffer "Org Project Name: "))
-         (org-dir
-          (read-directory-name "Org Project Directory: " (org-publishing-created-contents-path name)))
-         (publishing-path
-          (read-directory-name
-           "Org Project Publishing Directory: "
-           (org-publishing-generated-contents-path
-                   (replace-regexp-in-string (org-publishing-created-contents-path ) ""
-                                             (if (consp org-dir) (cl-first org-dir) org-dir)))))
+  (let* ((name    (read-from-minibuffer "Org Project Name: "))
+         (org-dir (read-directory-name "Org Project Directory: "
+                                       (org-publishing-created-contents-path name)))
+         (publishing-path (read-directory-name "Org Project Publishing Directory: "
+                                               (org-publishing-generated-contents-path (replace-regexp-in-string (org-publishing-created-contents-path)
+                                                                                                                 ""
+                                                                                                                 (if (consp org-dir) (cl-first org-dir) org-dir)))))
          (publishing-options nil))
-    `(
-      ,name
+    `(,name
       :base-directory ,org-dir
       :publishing-directory ,publishing-path
       ,@publishing-options)))
@@ -171,40 +161,39 @@
 ;;  (org-publishing-generated-contents-path "/web/site/blog/pdf")
 ;;  "ikiwiki"
 ;;  :base-url (org-publishing-website-address "/blog/"))
+
 
 ;;;###autoload
 (defun org-publish-get-attribute (project extention attrib)
   (let ((proj-alist (assoc project org-publish-project-alist)))
-    (or (plist-get (cl-rest proj-alist) attrib)
-        (plist-get (cl-rest (cl-first (remove-if-not (lambda (p)
-                                                 (string-match
-                                                  (plist-get (cl-rest p) :base-extension)
-                                                  extention))
-                                               (org-publish-expand-projects
-                                                (list proj-alist)))))
-                   attrib))))
+    (or (plist-get  (cl-rest proj-alist) attrib)
+        (plist-get  (cl-rest (cl-first (remove-if-not #'(lambda (p)
+                                                          (string-match (plist-get (cl-rest p) :base-extension)
+                                                                        extention))
+                                                      (org-publish-expand-projects (list proj-alist)))))
+                    attrib))))
 
 ;;;###autoload
 (defun org-publish-get-attribute (project extention attrib)
   ;; TODO: IMPROVE further
   (let ((proj-alist (assoc project org-publish-project-alist)))
-    (or
-     (plist-get (cl-rest proj-alist) attrib)
-     (let* ((projects
-             (mapcar #'car
-                     (remove-if-not (lambda (p)
-                                      (string-match
-                                       (plist-get (cl-rest p) :base-extension)
-                                       extention))
-                                    (org-publish-expand-projects
-                                     (list proj-alist)))))
-            (project
-             (find-if (lambda (p)
-                        (org-publish-get-attribute p extention attrib))
-              projects)))
-       (when project
-         (org-publish-get-attribute project extention attrib))))))
+    (or (plist-get (cl-rest proj-alist) attrib)
+        (let* ((projects (mapcar #'car
+                                 (remove-if-not (lambda (p)
+                                                  (string-match
+                                                   (plist-get (cl-rest p) :base-extension)
+                                                   extention))
+                                                (org-publish-expand-projects
+                                                 (list proj-alist)))))
+               (project (find-if #'(lambda (p) (org-publish-get-attribute p
+                                                                          extention
+                                                                          attrib))
+                                 projects)))
+          (when project
+            (org-publish-get-attribute project
+                                       extention
+                                       attrib))))))
 
 ;; (org-publish-get-attribute "tasks" "org" :base-directory)
-
+
 ;;; org-publishing.el ends here
