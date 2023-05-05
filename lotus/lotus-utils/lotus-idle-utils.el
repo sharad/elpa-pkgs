08373a154b2fb90186d1e801b389530b9b4223b1
Sharad Pratap <110339+sharad@users.noreply.github.com>
2023-05-05 19:16:25 +0530

    correction

--- lotus/lotus-utils/lotus-idle-utils.el
+++ lotus/lotus-utils/lotus-idle-utils.el
@@ -44,38 +44,33 @@
   ;; (when (memq (window-system frame) '(x w32 ns))
   ;;   (x-focus-frame frame))
   ;; Move mouse cursor if necessary.
-  (cond
-   (mouse-autoselect-window
-    (let ((edges (window-inside-edges (frame-selected-window frame))))
-      ;; Move mouse cursor into FRAME's selected window to avoid that
-      ;; Emacs mouse-autoselects another window.
-      (set-mouse-position frame (nth 2 edges) (nth 1 edges))))
-   (focus-follows-mouse
-    ;; Move mouse cursor into FRAME to avoid that another frame gets
-    ;; selected by the window manager.
-    (set-mouse-position frame (1- (frame-width frame)) 0))))
+  (cond (mouse-autoselect-window
+         (let ((edges (window-inside-edges (frame-selected-window frame))))
+           ;; Move mouse cursor into FRAME's selected window to avoid that
+           ;; Emacs mouse-autoselects another window.
+           (set-mouse-position frame (nth 2 edges) (nth 1 edges))))
+        (focus-follows-mouse
+         ;; Move mouse cursor into FRAME to avoid that another frame gets
+         ;; selected by the window manager.
+         (set-mouse-position frame (1- (frame-width frame)) 0))))
 
 (defun select-frame-set-input-focus-no-raise-p ()
   "Check if function select-frame-set-input-focus used by helm can raise frame?"
-  (and
-   (advice--p (advice--symbol-function 'select-frame-set-input-focus))
-   (advice-function-member-p
-    #'quiet--select-frame
-    (advice--symbol-function 'select-frame-set-input-focus))))
+  (and (advice--p (advice--symbol-function 'select-frame-set-input-focus))
+       (advice-function-member-p #'quiet--select-frame
+                                 (advice--symbol-function 'select-frame-set-input-focus))))
 
 (defun select-frame-set-input-enable-raise ()
   "Disable raising of frame by function select-frame-set-input-focus used by helm."
   (when (fboundp 'remove-function)
-    (remove-function
-     (symbol-function 'select-frame-set-input-focus)
-     #'quiet--select-frame)))
+    (remove-function (symbol-function 'select-frame-set-input-focus)
+                     #'quiet--select-frame)))
 
 (defun select-frame-set-input-disable-raise ()
   "Enable raising of frame by function select-frame-set-input-focus used by helm."
   (when (fboundp 'add-function)
-    (add-function
-     :override (symbol-function  'select-frame-set-input-focus)
-     #'quiet--select-frame)))
+    (add-function :override (symbol-function  'select-frame-set-input-focus)
+                  #'quiet--select-frame)))
 
 (defmacro with-select-frame-set-input-disable-raise (&rest body)
   `(unwind-protect
@@ -94,9 +89,8 @@
                         (progn
                           ,@body)
                       (remove-hook 'pre-command-hook (lambda () (funcall fun)))))))
-     (add-hook
-      'pre-command-hook
-      (lambda () (funcall fun)))))
+     (add-hook 'pre-command-hook
+               #'(lambda () (funcall fun)))))
 (put 'with-pre-command 'lisp-indent-function 0)
 
 (defmacro with-post-command (&rest body)
@@ -106,9 +100,8 @@
                          (progn
                            ,@body)
                        (remove-hook 'post-command-hook (lambda () (funcall fun)))))))
-     (add-hook
-      'post-command-hook
-      (lambda () (funcall fun)))))
+     (add-hook 'post-command-hook
+               #'(lambda () (funcall fun)))))
 (put 'with-post-command 'lisp-indent-function 0)
 
 (defmacro with-pre-command-local (&rest body)
@@ -118,9 +111,9 @@
                          (progn
                            ,@body)
                        (remove-hook 'pre-command-hook (lambda () (funcall fun)) t)))))
-     (add-hook
-      'pre-command-hook
-      (lambda () (funcall fun)) nil t)))
+     (add-hook 'pre-command-hook
+               #'(lambda () (funcall fun))
+               nil t)))
 (put 'with-pre-command-local 'lisp-indent-function 0)
 
 (defmacro with-post-command-local (&rest body)
@@ -130,9 +123,9 @@
                          (progn
                            ,@body)
                        (remove-hook 'post-command-hook (lambda () (funcall fun)) t)))))
-     (add-hook
-      'post-command-hook
-      (lambda () (funcall fun)) nil t)))
+     (add-hook 'post-command-hook
+               #'(lambda () (funcall fun))
+               nil t)))
 (put 'with-post-command-local 'lisp-indent-function 0)
 
 
