b590a3a0ad9bcf25ab0edc9e4571a6484ce85b46
Sharad Pratap <110339+sharad@users.noreply.github.com>
2023-07-16 18:56:07 +0530

    correction

--- lotus/occ/occ-commands.el
+++ lotus/occ/occ-commands.el
@@ -275,7 +275,9 @@
   (interactive (list (occ-util-select-from-sym-list "Select property: "
                                                     (occ-obj-properties-for-rank))))
   (require 'calc)
-  (occ-message "prop %s: %s" prop (math-format-flat-expr `(vec ,@(occ-obj-ineq-internal prop)) 1)))
+  (occ-message "prop %s: %s"
+               prop
+               (math-format-flat-expr `(vec ,@(occ-obj-ineq-internal prop)) 1)))
 
 
 ;;;###autoload
