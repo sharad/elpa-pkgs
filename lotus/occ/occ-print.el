f4c096aee4f01028707a432de8869af7106ca4db
Sharad Pratap <110339+sharad@users.noreply.github.com>
2023-10-11 01:24:28 +0530

    correction

--- lotus/occ/occ-print.el
+++ lotus/occ/occ-print.el
@@ -327,13 +327,6 @@ pointing to it."
                        rank
                        no-curr-clock
                        no-propterties)
-  ;; (message "Hello In")
-  ;; (prog1
-  ;;     (occ-obj-format obj 'capitalize
-  ;;                   rank
-  ;;                   no-curr-clock
-  ;;                   no-propterties)
-  ;;   (message "Hello Out"))
   (occ-obj-format obj 'capitalize
                   rank
                   no-curr-clock
