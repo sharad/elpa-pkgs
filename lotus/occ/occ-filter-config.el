d1b1670b6d87a4ef0287a12cde0146467808cb6f
Sharad Pratap <110339+sharad@users.noreply.github.com>
2023-10-25 20:39:29 +0530

    correction

--- lotus/occ/occ-filter-config.el
+++ lotus/occ/occ-filter-config.el
@@ -46,33 +46,33 @@
                                :rank-display-fn nil)
 
   (occ-obj-build-static-filter :positive
-                                "Positive"
-                                :points-gen-fn #'(lambda (ctx sequence &key rank)
-                                                   (list 0))
-                                :compare-fn #'>
-                                :default-pivot-fn #'(lambda (ctx points) 0)
-                                :rank-select-fn  nil
-                                :rank-display-fn nil)
+                               "Positive"
+                               :points-gen-fn #'(lambda (ctx sequence &key rank)
+                                                  (list 0))
+                               :compare-fn #'>
+                               :default-pivot-fn #'(lambda (ctx points) 0)
+                               :rank-select-fn  nil
+                               :rank-display-fn nil)
 
   (occ-obj-build-static-filter :non-negative
-                                "Non-Negative"
-                                :points-gen-fn #'(lambda (ctx sequence &key rank)
-                                                   (list -1))
-                                :compare-fn #'>
-                                :default-pivot-fn #'(lambda (ctx points)
-                                                      (/ (length points) 2))
-                                :rank-select-fn  nil
-                                :rank-display-fn nil)
+                               "Non-Negative"
+                               :points-gen-fn #'(lambda (ctx sequence &key rank)
+                                                  (list -1))
+                               :compare-fn #'>
+                               :default-pivot-fn #'(lambda (ctx points)
+                                                     (/ (length points) 2))
+                               :rank-select-fn  nil
+                               :rank-display-fn nil)
 
   (occ-obj-build-static-filter :identity
-                                "Identity"
-                                :points-gen-fn #'(lambda (ctx sequence &key rank)
-                                                   (list 0))
-                                :compare-fn #'(lambda (rank pivot) t)
-                                :default-pivot-fn #'(lambda (obj points)
-                                                      0)
-                                :rank-select-fn  nil
-                                :rank-display-fn nil))
+                               "Identity"
+                               :points-gen-fn #'(lambda (ctx sequence &key rank)
+                                                  (list 0))
+                               :compare-fn #'(lambda (rank pivot) t)
+                               :default-pivot-fn #'(lambda (obj points)
+                                                     0)
+                               :rank-select-fn  nil
+                               :rank-display-fn nil))
 
 
 ;; Filter should be list of keys or cons of key and customized rank function
