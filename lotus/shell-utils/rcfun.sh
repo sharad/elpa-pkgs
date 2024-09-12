320099f56259d48c15646d81ba7f334a6af3a50e
Sharad Pratap <spratap@fortinet.com>
2024-09-12 18:35:26 +0530

    correction

---
+++ lotus/shell-utils/rcfun.sh
@@ -0,0 +1,61 @@
+# -*- Mode: shell-script; indent-tabs-mode: nil; sh-shell: zsh; -*-
+
+
+##{{ emacs
+# from: https://jpace.wordpress.com/2016/12/01/current-file-and-directory-in-emacs-and-z-shell/
+
+function emacs-call-function() {
+    local funname=$1
+    echo ${(Q)~$(emacsclient -f ~/.emacs.d/server/$EMACS_SERVER_NAME \
+                             -w 2                                    \
+                             -e "(${funname})" |                     \
+                     sed -e '1!b' -e '/emacsclient: connected to remote socket at/d')}
+}
+
+function emacs-current-filename() {
+    local _location="$(emacs-call-function shell-current-buffer-filename)"
+    echo "${_location}"
+}
+function emacs-current-filename-follow() {
+    local _location="$(emacs-call-function shell-current-buffer-filename-truename)"
+    echo "${_location}"
+}
+function emacs-current-defaultdir() {
+    local _location="$(emacs-call-function shell-current-buffer-directory)"
+    echo "${_location}"
+}
+function emacs-current-defaultdir-follow() {
+    local _location="$(emacs-call-function shell-current-buffer-directory-truename)"
+    echo "${_location}"
+}
+#
+function emacs-chdir-defaultdir() {
+    local _location="$(emacs-current-defaultdir)"
+    if [ "$_location" ]
+    then
+        cd "${_location}"
+    else
+        echo no dir
+    fi
+}
+function emacs-chdir-defaultdir-follow() {
+    local _location="$(emacs-current-defaultdir-follow)"
+    if [ "$_location" ]
+    then
+        cd "${_location}"
+    else
+        echo no dir
+    fi
+}
+
+
+alias ecf=emacs-current-filename
+alias ecff=emacs-current-filename-follow
+alias ecd=emacs-current-defaultdir
+alias ecdf=emacs-current-defaultdir-follow
+#
+alias cde=emacs-chdir-defaultdir
+alias cdef=emacs-chdir-defaultdir-follow
+##}}
+
+
---
+++ lotus/shell-utils/shell-utils.el
@@ -0,0 +1,53 @@
+;;; shell-utils.el --- shell function callables      -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  Music Player Daemon (MPD) user
+
+;; Author: Music Player Daemon (MPD) user <spratap@merunetworks.com>
+;; Keywords: convenience, files, comm, tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(provide 'shell-utils)
+
+
+;;;###autoload
+(defun shell-current-buffer-filename ()
+  (with-current-buffer (window-buffer (selected-window))
+    buffer-file-name))
+
+;;;###autoload
+(defun shell-current-buffer-filename-truename ()
+  (with-current-buffer (window-buffer (selected-window))
+    (when buffer-file-name
+      (file-truename buffer-file-name))))
+
+;;;###autoload
+(defun shell-current-buffer-directory ()
+  (interactive)
+  (with-current-buffer (window-buffer (selected-window))
+    default-directory))
+
+;;;###autoload
+(defun shell-current-buffer-directory-truename ()
+  (interactive)
+  (with-current-buffer (window-buffer (selected-window))
+    (file-name-directory (file-truename (buffer-file-name)))))
+
+;;; shell-utils.el ends here
