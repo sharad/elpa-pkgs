a78459fcba8833bbd03a309274916594f5382286
Sharad Pratap <110339+sharad@users.noreply.github.com>
2025-01-13 20:18:54 +0530

    correction

--- lotus/sessions-unified/sessions-unified-tab-bar.el
+++ lotus/sessions-unified/sessions-unified-tab-bar.el
@@ -62,6 +62,67 @@ Return its existing value or a new value."
 (defun tab-bar-tabs-set (tabs &optional frame)
   "Set a list of TABS on the FRAME."
   (set-frame-parameter frame 'tabs tabs))
+
+
+;; (tab-bar--tab)
+
+;; (tab-bar-tabs)
+
+
+
+(defun tab-bar--tab (&optional frame)
+  "Make a new tab data structure that can be added to tabs on the FRAME."
+  (let* ((tab (tab-bar--current-tab-find nil frame))
+         (tab-explicit-name (alist-get 'explicit-name tab))
+         (tab-group (alist-get 'group tab))
+         (bl  (seq-filter #'buffer-live-p (frame-parameter
+                                           frame 'buffer-list)))
+         (bbl (seq-filter #'buffer-live-p (frame-parameter
+                                           frame 'buried-buffer-list))))
+    `(tab
+      (name . ,(if tab-explicit-name
+                   (alist-get 'name tab)
+                 (funcall tab-bar-tab-name-function)))
+      (explicit-name . ,tab-explicit-name)
+      ,@(if tab-group `((group . ,tab-group)))
+      (time . ,(float-time))
+      (ws . ,(window-state-get
+              (frame-root-window (or frame (selected-frame))) 'writable))
+      (wc . ,(current-window-configuration))
+      (wc-point . ,(point-marker))
+      (wc-bl . ,bl)
+      (wc-bbl . ,bbl)
+      ,@(when tab-bar-history-mode
+          `((wc-history-back . ,(gethash (or frame (selected-frame))
+                                         tab-bar-history-back))
+            (wc-history-forward . ,(gethash (or frame (selected-frame))
+                                            tab-bar-history-forward))))
+      ;; Copy other possible parameters
+      ,@(mapcan (lambda (param)
+                  (unless (memq (car param)
+                                '(name explicit-name group time
+                                       ws wc wc-point wc-bl wc-bbl
+                                       wc-history-back wc-history-forward))
+                    (list param)))
+                (cdr tab)))))
+
+
+
+(defun my-serialize-window-configuration ()
+  "Serialize the current window configuration as an S-expression."
+  (let ((windows nil))
+    (dolist (win (window-list))
+      (let ((buffer (buffer-name (window-buffer win)))
+            (point (window-point win))
+            (start (window-start win))
+            (parameters (window-parameters win)))
+        (push (list buffer point start parameters) windows)))
+    (nreverse windows)))
+
+
+;; (current-window-configuration)
+(window-configuration-get (current-window-configuration) 'windows)
+
 
 
 
