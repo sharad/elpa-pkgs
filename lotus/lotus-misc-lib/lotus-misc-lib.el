334d5ff0d4fb3c4f3b7f388bddb900a526992e3e
Sharad Pratap <110339+sharad@users.noreply.github.com>
2025-01-16 18:24:04 +0530

    autosave lotus/lotus-misc-lib/lotus-misc-lib.el after save

--- lotus/lotus-misc-lib/lotus-misc-lib.el
+++ lotus/lotus-misc-lib/lotus-misc-lib.el
@@ -1070,7 +1070,7 @@ to see whether it should be considered."
 (advice-add 'magit-wip-commit-worktree
             :around #'magit-wip-commit-worktree-around-advice-fn)
 (advice--p #'magit-wip-commit-worktree)
-
+
 
 
 
