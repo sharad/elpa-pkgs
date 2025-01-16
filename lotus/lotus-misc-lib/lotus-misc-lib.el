4eba047eeb369978fd642d51a44be59b5cd6916d
Sharad Pratap <110339+sharad@users.noreply.github.com>
2025-01-16 17:53:37 +0530

    autosave lotus/lotus-misc-lib/lotus-misc-lib.el after save

--- lotus/lotus-misc-lib/lotus-misc-lib.el
+++ lotus/lotus-misc-lib/lotus-misc-lib.el
@@ -1058,7 +1058,8 @@ to see whether it should be considered."
   (message "magit-wip-push: ref %s, files %s, msg %s"
            ref files msg)
 
-  (let ((wip-ref (concat "ref/wip/wtree/" ref)))
+  (let ((branch  ())
+        (wip-ref (concat "ref/wip/wtree/" ref)))
     (magit-ref-p wip-ref))
 
   (message "upstream branch: %s"
