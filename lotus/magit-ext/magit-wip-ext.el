c841d56b62b4ddae95d7b7fc979a25571ec5bac5
Sharad Pratap <110339+sharad@users.noreply.github.com>
2025-01-21 18:47:07 +0530

    autosave lotus/magit-ext/magit-wip-ext.el after save

--- lotus/magit-ext/magit-wip-ext.el
+++ lotus/magit-ext/magit-wip-ext.el
@@ -68,6 +68,7 @@ Optional argument REMOTE can specify the remote explicitly."
 
 
 
+
 (defun magit-ext-git-push-nons (branch target args)
   (run-hooks 'magit-credential-hook)
   ;; If the remote branch already exists, then we do not have to
