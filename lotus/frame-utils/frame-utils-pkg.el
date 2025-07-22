43e194286d1adcf795a45ae9617d808707715095
Sharad Pratap <110339+sharad@users.noreply.github.com>
2025-07-22 17:56:46 +0530

    correction

--- lotus/frame-utils/frame-utils-pkg.el
+++ lotus/frame-utils/frame-utils-pkg.el
@@ -1,4 +1,3 @@
 (define-package "frame-utils" "20171222.126" "frame-utils"
   '((buffer-utils "1")
-    ;; (elscreen     "1")
     ))
--- lotus/lotus-utils/lotus-utils-pkg.el
+++ lotus/lotus-utils/lotus-utils-pkg.el
@@ -1,3 +1,2 @@
 (define-package "lotus-utils" "20190821.2349" "lotus-utils"
-  '(;; (elscreen "1")
-    ))
+  '())
